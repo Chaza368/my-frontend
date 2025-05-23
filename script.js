97ddc07cd6fe1180cfa0205c4033b79e0515affe
Chaza368 <charliestanley368@gmail.com>
2025-05-24 00:11:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.getElementById('btn').addEventListener('click', () => {
+  fetch('INSERTURLHERE/api/data')
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById('output').textContent = JSON.stringify(data, null, 2);
+    })
+    .catch(err => {
+      document.getElementById('output').textContent = 'Error: ' + err;
+    });
+});
 
