31aab60665e4a37cdb9b7044baf215765b1f6d75
Chaza368 <charliestanley368@gmail.com>
2025-05-24 12:34:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('btn').addEventListener('click', () => {
   fetch('https://1e061d7b-265d-4e30-a80b-0e3c4d039c34-00-1vsx176a6brhg.janeway.replit.dev/api/data')
     .then(res => res.json())
     .then(data => {
-      document.getElementById('output').textContent = JSON.stringify(data, null, 2);
+      document.getElementById('output').textContent = JSON.stringify(data.message, null, 2);
     })
     .catch(err => {
       document.getElementById('output').textContent = 'Error: ' + err;
