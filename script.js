8dff170cc89b362be6281125779a658ed3b2714e
Chaza368 <charliestanley368@gmail.com>
2025-05-24 13:07:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-document.getElementById('btn').addEventListener('click', () => {
+document.getElementById('btn').addEventListener('click', function() {
+  this.style.backgroundColor = 'green';
+});
   fetch('https://1e061d7b-265d-4e30-a80b-0e3c4d039c34-00-1vsx176a6brhg.janeway.replit.dev/api/data')
     .then(res => res.json())
     .then(data => {
